35191fa0416a09271dcf7b754e8754cdaa261b8b
primayoriko <primayoriko@gmail.com>
2019-11-24 14:00:56 +0700

    '1'

--- parser.py
+++ parser.py
@@ -3,3 +3,9 @@
 # 1. Jun Ho Choi Hedyatmo / 13518044
 # 2. Naufal Prima Yoriko / 13518146
 # 3. Stefanus Gusega Gunawan / 13518149
+import sys
+import re
+import pandas as pd
+
+class Parser:
+    def 
