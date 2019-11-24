af4c30882bc437ed962157c3baa76de0cb61dcbe
stefanusgusega <gunawanstefanus006@gmail.com>
2019-11-24 11:44:53 +0700

    bikin file buat koding dipisah-pisah

---
+++ cyk.py
@@ -0,0 +1,5 @@
+# Ini untuk CYK #
+# Kelompok : The Godfather #
+# 1. Jun Ho Choi Hedyatmo / 13518044
+# 2. Naufal Prima Yoriko / 13518146
+# 3. Stefanus Gusega Gunawan / 13518149
---
+++ parser.py
@@ -0,0 +1,5 @@
+# Ini untuk Parser #
+# Kelompok : The Godfather #
+# 1. Jun Ho Choi Hedyatmo / 13518044
+# 2. Naufal Prima Yoriko / 13518146
+# 3. Stefanus Gusega Gunawan / 13518149
